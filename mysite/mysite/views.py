192caf23b4eac05ca7a86068e0f51c4866d74fb4
julias16 <juliasjisan17@gmail.com>
2025-09-25 23:26:28 +0600

    Repository Created

---
+++ mysite/mysite/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+def aboutus(request):
+    return render(request, 'aboutus.html')
+def clothsfirst(request):
+    return render(request, 'clothsfirst.html')
+def contactus(request):
+    return render(request, 'contactus.html')
+def furniturefirst(request):
+    return render(request, 'furniturefirst.html')
+def foodfirst(request):
+    return render(request, 'foodfirst.html')
+def login(request):
+    return render(request, 'login.html')
+def signup(request):
+    return render(request, 'signup.html')
+def donateform(request):
+    return render(request, 'donateform.html')
+def blog(request):
+    return render(request, 'blog.html')
